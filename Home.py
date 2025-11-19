9b4b43d3b5c42ecfc37b23586c334bfa8e347b25
Beemmmm <664230026@webmail.npru.ac.th>
2025-11-19 15:28:34 +0700

    .
    .

--- Home.py
+++ Home.py
@@ -4,6 +4,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 st.header("Yonlada")
 st.image("./pic/beam.jpg")
+
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./pic/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./pic/iris3.jpg")
